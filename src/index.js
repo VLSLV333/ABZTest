dd227e22fccb4764f28039d5b32ceeb92433d5c8
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-03-19 23:08:47 +0200

    moved worker, probably it wount work...

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'react-tooltip/dist/react-tooltip.css';
-import * as serviceWorker from '../public/service-worker';
+import * as serviceWorker from './service-worker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
