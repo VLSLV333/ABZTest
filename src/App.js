28e12a0759a04100219c08f758532024fa117de6
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-03-19 03:13:23 +0200

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import Navigation from './components/navigation/Navigation';
-import Header from './components/header/Header';
-import MainSection from './components/main/Mainsection';
-import Footer from './components/footer/Footer';
+import { lazy, Suspense } from 'react';
+
 import SubmitProvider from './store/SubmitProvider';
-import Modal from './components/modal/Modal';
+
+const Modal = lazy(() => import('./components/modal/Modal'));
+const Navigation = lazy(() => import('./components/navigation/Navigation'));
+const Header = lazy(() => import('./components/header/Header'));
+const MainSection = lazy(() => import('./components/main/Mainsection'));
+const Footer = lazy(() => import('./components/footer/Footer'));
 
 function App() {
 	return (
 		<SubmitProvider>
-			<Modal />
-			<Navigation />
-			<Header />
-			<MainSection />
-			<Footer />
+			<Suspense>
+				<Modal />
+			</Suspense>
+			<Suspense>
+				<Navigation />
+			</Suspense>
+			<Suspense>
+				<Header />
+			</Suspense>
+			<Suspense>
+				<MainSection />
+			</Suspense>
+			<Suspense>
+				<Footer />
+			</Suspense>
 		</SubmitProvider>
 	);
 }
