b42584e0f751a42cb4acdc45fbd140c43f7a97ec
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-03-19 23:27:46 +0200

    worker is gone)

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'react-tooltip/dist/react-tooltip.css';
-import * as serviceWorker from './service-worker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode> 
+	</React.StrictMode>
 );
-
-serviceWorker.register();
